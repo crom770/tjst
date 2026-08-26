11cbc5e875f8c3a724254375e882f7f5b752f2ed
crom770 <leekh@tjst.co.kr>
2026-08-26 22:27:36 +0900

    Update BootReceiver.kt

--- app/src/main/java/com/tjst/multilangplayer/BootReceiver.kt
+++ app/src/main/java/com/tjst/multilangplayer/BootReceiver.kt
@@ -3,14 +3,17 @@ package com.tjst.multilangplayer
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            val activityIntent = Intent(context, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            val serviceIntent = Intent(context, BootService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
             }
-            context.startActivity(activityIntent)
         }
     }
 }
